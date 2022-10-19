7a82b16a7cfd13de5a492b4cba9978e295f7a259
s4shivli <s4shivli@gmail.com>
2022-10-20 00:09:16 +0100

    Initial commit

---
+++ src/main/java/com/dove/card/CreditCardApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dove.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dove/card/CreditCardApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dove.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
